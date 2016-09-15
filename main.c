bbf8417a6582bf82e84aca5792b1b94e3eea2d50
Dylan Way <dylan.way@colorado.edu>
2016-09-14 22:58:37 -0600

    changed the way the strings were printed

--- main.c
+++ main.c
@@ -6,26 +6,31 @@
 int main()
 {
 	int error = 0;
+	int i;
 	
-	char array1[18] = "This is a string.\0";
-	char array2[19] = "some NUMmbers12345\0";
-	char array3[31] = "Does it reverse \n\0\t correctly?\0";
+	char array1[17] = "This is a string.";
+	char array2[18] = "some NUMmbers12345";
+	char array3[30] = "Does it reverse \n\0\t correctly?";
 	
-	printf("Before: %s\n", array1);
 	error = reverse(array1, 17);
-	printf("After: %s\n", array1);
+	for(i = 0; i < 17; i++) {
+		printf("%c", *(array1+i));
+	}
+	printf("\n");
+
+
+	error = reverse(array2, 18);
+	for(i = 0; i < 18; i++) {
+		printf("%c", *(array2+i));
+	}
+	printf("\n");
 	
-	if (!error) {	
-		printf("Before: %s\n", array2);
-		error = reverse(array2, 18);
-		printf("After: %s\n", array2);
-	}			
+	error = reverse(array3, 30);
+	for(i = 0; i < 30; i++) {
+		printf("%c", *(array3+i));
+	}
+	printf("\n");
 	
-	if (!error) {	
-		printf("Before: %s\n", array3);
-		error = reverse(array3, 30);
-		printf("After: %s\n", array3);
-	}			
 	//returns the error code	
 	return error;
 }
