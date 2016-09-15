4e71d8e60a5374ad57ab6966cec6e37d43ad49de
Dylan Way <dylan.way@colorado.edu>
2016-09-14 21:45:53 -0600

    Added the hw1.c, hw1.h, and main.c files

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "hw1.h"
+
+/*Main initializes the array, calls reverse, and checks the final array with another
+  array corresponding to the original message. */
+int main()
+{
+	int error = 0;
+	
+	char array1[17] = "This is a string.";
+	char array2[18] = "some NUMmbers12345";
+	char array3[30] = "Does it reverse \n\0\t correctly?";
+	
+	printf("Before: %s\n", array1);
+	error = reverse(array1, 17);
+	printf("After: %s\n", array1);
+	
+	if (!error) {	
+		printf("Before: %s\n", array2);
+		error = reverse(array2, 18);
+		printf("After: %s\n", array2);
+	}			
+	
+	if (!error) {	
+		printf("Before: %s\n", array3);
+		error = reverse(array3, 30);
+		printf("After: %s\n", array3);
+	}			
+	//returns the error code	
+	return error;
+}
